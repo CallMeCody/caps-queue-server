361afa7ba565c3dfb40961c682304ae3ea529ab1
Cody Carpenter <cody_carpenter2222@yahoo.com>
2021-04-03 21:30:45 -0700

    done

--- queue-server.js
+++ queue-server.js
@@ -1,1 +1,13 @@
+'use strict';
 
+const port = process.env.PORT || 3000;
+
+const io = require('socket.io')(port);
+const host = 'http://localhost:3000'
+
+const socket = io.connect(host)
+const caps = io.connect(`${host}/caps`);
+
+const messageQueue = {
+
+};
